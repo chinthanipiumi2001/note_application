1c3f8b7664916d0779bb9000cf82001f7c48020d
user <dissanayakepiumi75@gmail.com>
2024-02-13 19:22:12 +0530

    add comments room database

--- app/src/main/java/com/example/note_application/Notes/Notes.kt
+++ app/src/main/java/com/example/note_application/Notes/Notes.kt
@@ -2,6 +2,7 @@ package com.example.note_application.Notes
 
 import androidx.room.Entity
 import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 
 //this will create an entity table
 //entity class is data class
@@ -11,4 +12,9 @@ import androidx.room.ColumnInfo
 @Entity(tableName ="notes_table")
 //our table will have
  data class Notes(@ColumnInfo(name ="text")val text:String) {
+  //we will access data according to the id of the objet
+  //we want id to be auto generate
+  @PrimaryKey(autoGenerate = true)var id =0
 }
+//this is our entity
+//now let's create
