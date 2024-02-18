b4856d6c5e248d2516e902f3c45a320d976b46d9
user <dissanayakepiumi75@gmail.com>
2024-02-18 23:23:05 +0530

    add instance

--- app/src/main/java/com/example/note_application/Notes/NotesDatabase.kt
+++ app/src/main/java/com/example/note_application/Notes/NotesDatabase.kt
@@ -1,12 +1,36 @@
 package com.example.note_application.Notes
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 //Room database are abstract class
 //annotate the database
 //we have create entity
-//
-@Database(entities = [Notes::class], version =1, exportSchema = false)
-abstract class NotesDatabase:RoomDatabase() {
+@Database([Notes::class], version =1, exportSchema = false)
+abstract class NotesDatabase:RoomDatabase(){
+    abstract fun getNoteDao(): Notes
+    //we will make a single instance of room database so it prevent multiple opening of database at same time
+
+    companion object {
+        // Singleton instance of the database
+        @Volatile
+        private var INSTANCE: NotesDatabase? = null
+        fun getDatabase(context:Context):NotesDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instantce = Room.databaseBuilder(
+                    context.applicationContext,
+                    NotesDatabase::class.java,
+                    "note database"
+                ).build()
+                INSTANCE =instantce
+                instantce
+            }
+        }
+    }
 }
+
+
+
+
